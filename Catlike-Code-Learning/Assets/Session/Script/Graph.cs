cc9d4abae43ca2f46002fc0b5bf2570ebbe9ab9e
Jie Guan <60665347+jieguann@users.noreply.github.com>
2023-04-11 00:13:36 -0400

    updated

--- Catlike-Code-Learning/Assets/Session/Script/Graph.cs
+++ Catlike-Code-Learning/Assets/Session/Script/Graph.cs
@@ -23,12 +23,18 @@ namespace Graph
             var scale = Vector3.one * step;
 
             points = new Transform[resolution * resolution];
-            for (int i = 0; i < points.Length; i++)
+            for (int i = 0, x = 0, z=0; i < points.Length; i++, x++)
             {
+                if (x == resolution)
+                {
+                    x = 0;
+                    z += 1;
+                }
                 Transform point = points[i] = Instantiate(pointPrefab);
                 point.SetParent(transform, false);
 
-                position.x = (i + 0.5f)  * step - 1f;
+                position.x = (x + 0.5f)  * step - 1f;
+                position.z = (z + 0.5f) * step - 1f;
                 //position.y = position.x * position.x;
                 point.localPosition = position;
                 point.localScale = scale;
@@ -43,6 +49,7 @@ namespace Graph
             float time = Time.time;
             for (int i = 0; i < points.Length; i++)
             {
+                
                 Transform point = points[i];
                 Vector3 position = point.localPosition;
                 position.y = f(position.x, position.z, time);
